25f4dc42da852c459b1a61420aad26d80d94e984
Zhang Chong <zcychongyao2008@sina.com>
2018-09-04 22:06:03 +0800

    vip选项卡css3

--- js/vip.js
+++ js/vip.js
@@ -1,9 +1,8 @@
 $(function () {
-
 //选项卡切换样式
     var tabs=$("#tabs_cards li");
-    var list=[]
     var statuscode=0
+    var product_list=[]
     tabs.click(function () {
         $(this).addClass("active").siblings().removeClass('active')
         $("#tab_body .cards").eq($(this).index()).show().siblings().hide()
@@ -32,15 +31,17 @@ $(function () {
     }
 
     function  renderProduct(list) {
-        for (let i = 0; i <6 ; i++) {
+            product_list=list
+        for (let i = 0; i <list.length ; i++) {
             console.log(list[i].ce_productName)
-            $(".cards1 ul").append(`<li>
-                            <div class="wz">
+            $(".cards1 ul").append(`<li style="background: url('${list[i].ce_product_icon}')  no-repeat ;">
+                            <div class="wz" >
                                 <div>${list[i].ce_productName}</div>
                                 <div>${list[i].ce_product_now_price}/年</div>
                             </div>
                         </li>`)
         }
+        $(".cards1 ul li").eq(0).addClass("hover").siblings().removeClass("hover")
         $(".vip-detail .vd").append(`<h4 class="h4">${list[statuscode].ce_productName} <span >
 ${list[statuscode].ce_product_now_price}/年<i style="font-size: 14px;text-decoration: line-through;
 padding-left: 10px;color: #888 ">原价${list[statuscode].ce_product_original_price}/年</i></span></h4>
