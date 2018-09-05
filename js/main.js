c1e065b27ee9dd35c7e1a08f956eff24f8cb1698
Zhang Chong <zcychongyao2008@sina.com>
2018-09-05 14:00:20 +0800

    首页菜单接口对接渲染

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var menuData=[]
+
 var mySwiper = new Swiper ('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -16,28 +18,73 @@ var mySwiper = new Swiper ('.swiper-container', {
     }
 })
 
-
+/*请求菜单内容*/
+$.ajax(
+    {
+        type: 'get',
+        url: `http://192.168.0.115:8080/ycweb/first/find_All_Menu.do`,
+        dataType: 'jsonp',
+        jsonpCallback: "callback",
+        success: function (data) {
+        menuData=data;
+            for(let i in data){
+                // console.log(data[i])
+                $(".child-menu").append(`<li key="${i}">
+                    <div class="title">${data[i].ft_category_name}</div>
+                    <div class="bar-content">
+                    </div>
+                    <div class="menu-line"></div></li>`)
+                    for (let j in data[i].second) {
+                        $(".bar-content").append(`<span>${data[i].second[j].se_category_name}</span>`)
+                    }
+                    console.log(data[i].ft_category_name)
+                    console.log(data[i].second)
+            }
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    }
+);
 /*三级菜单展示*/
-    var menu_item = $(".child-menu li")
+    var menu_item = $(".child-menu")
 
-    menu_item.mouseout(function () {
-        $(".children-menu").mouseout(function () {
-            $(".children-menu").hide()
-        })
 
-    })
-
-menu_item.bind("mouseover",function () {
+$(".child-menu").on("mouseover","li",function () {
+    let key=$(this).attr("key")
+    let menuList=menuData[key].second
+    if($(".pad-l").children().length<=0){
+        for (let i in menuList) {
+            $(".pad-l").append(`<div class="pl-item left">
+                    <h4 class="p-title">${menuList[i].se_category_name}</h4>
+                    <div class="p-link">
+                    </div>
+                </div>`)
+            for (let j in menuList[i].third) {
+                $(".p-link").append(`<span><a href="">${menuList[i].third[j].th_category_ame}</a></span>`)
+            }
+        }
+    }
     $(".children-menu").show()
 })
-menu_item.bind("mouseout",function () {
-    $(".children-menu").hide()
-})
-$(".children-menu").bind("mouseover",function () {
-    $(".children-menu").show()
-})
-$(".children-menu").bind("mouseout",function () {
-    $(".children-menu").hide()
-})
+// menu_item.on("mouseout",function () {
+//     $(".children-menu").mouseout(function () {
+//         $(".children-menu").hide()
+//     })
+// })
+// menu_item.on("mouseout","li",function () {
+//     $(".pad-l").html("")
+//     $(".children-menu").hide()
+// })
+// $(".children-menu").on("mouseover",function () {
+//     $(".children-menu").show()
+// })
+// $(".children-menu").on("mouseout",function () {
+//     $(".children-menu").hide()
+// })
 
+/*tabs切换*/
+$("#tabs li").click(function () {
+    $(this).addClass("tabs-active").siblings().removeClass("tabs-active")
+})
 
--- js/vip.js
+++ js/vip.js
@@ -1,25 +1,29 @@
 $(function () {
 //选项卡切换样式
-    var tabs=$("#tabs_cards li");
-    var statuscode=0
-    var product_list=[]
+    var tabs = $("#tabs_cards li");
+    var statuscode = 0
+    var product_list = []
     tabs.click(function () {
         $(this).addClass("active").siblings().removeClass('active')
         $("#tab_body .cards").eq($(this).index()).show().siblings().hide()
     })
-
+    /*tabs切换*/
+    $("#tabs li").click(function () {
+        $(this).addClass("tabs-active").siblings().removeClass("tabs-active")
+    })
 //鼠标经过卡片时的动画
-    $(".cards1 ul").on("mouseover","li",function () {
+    $(".cards1 ul").on("mouseover", "li", function () {
         $(this).addClass("hover").siblings().removeClass("hover")
         $(this).siblings().find(".wz").removeClass("wz-hover")
         $(this).find(".wz").addClass("wz-hover").siblings().removeClass("wz-hover")
     })
     request_products()
-    function request_products(){
+
+    function request_products() {
         $.ajax(
             {
                 type: 'get',
-                url:`http://192.168.0.115:8080/ycweb/product/find_All_product.do`,
+                url: `http://192.168.0.115:8080/ycweb/product/find_All_product.do`,
                 dataType: 'jsonp',
                 jsonpCallback: "callback",
                 success: renderProduct,
@@ -30,9 +34,9 @@ $(function () {
         );
     }
 
-    function  renderProduct(list) {
-            product_list=list
-        for (let i = 0; i <list.length ; i++) {
+    function renderProduct(list) {
+        product_list = list
+        for (let i = 0; i < list.length; i++) {
             console.log(list[i].ce_productName)
             $(".cards1 ul").append(`<li style="background: url('${list[i].ce_product_icon}')  no-repeat ;">
                             <div class="wz" >
@@ -43,13 +47,14 @@ $(function () {
         }
         $(".cards1 ul li").eq(0).addClass("hover").siblings().removeClass("hover")
         $(".vip-detail .vd").append(`<h4 class="h4">${list[statuscode].ce_productName} <span >
-${list[statuscode].ce_product_now_price}/年<i style="font-size: 14px;text-decoration: line-through;
-padding-left: 10px;color: #888 ">原价${list[statuscode].ce_product_original_price}/年</i></span></h4>
+        ${list[statuscode].ce_product_now_price}/年<i style="font-size: 14px;text-decoration: line-through;
+        padding-left: 10px;color: #888 ">原价${list[statuscode].ce_product_original_price}/年</i></span></h4>
                     <div class="detail-body">
                         <span>功能：</span><p>${list[statuscode].ce_product_desc}</p>
                     </div>`)
     }
-    function toggle_img(){
+
+    function toggle_img() {
         console.log($(this))
     }
 })
